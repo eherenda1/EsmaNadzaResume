b0893196917b254f42f65796bb4c434a31ae050b
Nadza Buhic <nadza_buhic@zira>
2019-10-16 13:57:37 +0200

    nesto

--- EsmaNadzaApp/src/app/app-routing.module.ts
+++ EsmaNadzaApp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {StartComponent} from './pages/start/start.component'
+import {StartComponent } from './pages/start/start.component';
+
 
 
 const routes: Routes = [
   {path: '', component:StartComponent},
+
  /*{path: 'glavna/:name', component:GlavnaComponent}*/
 ];
 
