5d43e788fbb790d9bbc3bacd51a1f3dfa030ff00
Nadza Buhic <nadza_buhic@zira>
2019-10-16 10:32:14 +0200

    rutiranja 2. dio

---
+++ EsmaNadzaApp/src/app/Resume page/resume.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'resume-page',
+  templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.scss']
+})
+export class ResumeComponent {
+  
+}
--- EsmaNadzaApp/src/app/app.module.ts
+++ EsmaNadzaApp/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {StartComponent} from './Start Page/start.component'
+import { StartComponent } from './Start Page/start.component';
+import { ResumeComponent } from './Resume Page/resume.component';
+import { ContactComponent } from './Contact Page/contact.component';
+import { ProjectComponent } from './Project Page/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartComponent
+    StartComponent,
+    ResumeComponent,
+    ContactComponent, 
+    ProjectComponent
+
   ],
   imports: [
     BrowserModule,
