5f15335319db0edc661367c4d41d426da3090ca4
Esma Herenda <esma_herenda@zira>
2019-10-16 13:49:31 +0200

    rutiranje

--- EsmaNadzaApp/src/app/app-routing.module.ts
+++ EsmaNadzaApp/src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {StartComponent} from './pages/start/start.component'
+import { LayoutComponent } from './pages/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProjectComponent } from './pages/project/project.component';
+
+import {HeaderComponent} from './pages/layout/components/header/header.component';
+import {FooterComponent} from './pages/layout/components/footer/footer.component';
 
 
 const routes: Routes = [
   {path: '', component:StartComponent},
- /*{path: 'glavna/:name', component:GlavnaComponent}*/
+  { path: 'layout', 
+    component:LayoutComponent,
+    children: [
+      { path:'home', component:HomeComponent},
+      { path:'resume', component:ResumeComponent},
+      { path:'projects', component:ProjectComponent},
+      { path:'contact', component:ContactComponent},
+    ]
+  }
+
 ];
 
 @NgModule({
