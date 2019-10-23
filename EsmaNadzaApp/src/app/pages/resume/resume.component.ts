35065b2ff4240c586190a83d1d54fc481f72cf07
Nadza Buhic <nadza_buhic@zira>
2019-10-23 13:14:24 +0200

    u progresu

--- EsmaNadzaApp/src/app/pages/resume/resume.component.ts
+++ EsmaNadzaApp/src/app/pages/resume/resume.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ResumeService } from 'src/app/resume.service';
 import { ActivatedRoute } from "@angular/router";
-import { IResume } from 'src/app/resume';
+import { IResume, IWork, IHobbiesInterests } from 'src/app/resume';
 
 
 @Component({
@@ -12,13 +12,30 @@ import { IResume } from 'src/app/resume';
 export class ResumeComponent implements OnInit {
   public code: string;
   public resume: IResume;
+  public workE: IWork[];
+  public hobbiesInterestsE: IHobbiesInterests[];
+  public readMore: boolean;
+
   constructor(private _resumeService: ResumeService, private route: ActivatedRoute) {
 
+  }
+  
+  wants2readMore(){
+    this.readMore = !this.readMore;
   }
   ngOnInit(){
     this.route.parent.params.subscribe((params:any) => {
       this.code = params.code;
     })
-   this._resumeService.getResume(this.code).subscribe((data)=> this.resume = data);
+   this._resumeService.getResume(this.code).subscribe((data)=> {
+     this.resume = data;
+     this.workE = this.resume.work;
+     this.hobbiesInterestsE = this.resume.hobbiesInterests;
+   });
+   
+   
+  // this._resumeService.getResume(this.code).subscribe((data)=> this.resume.work = data);
+
+   
   }
 }
