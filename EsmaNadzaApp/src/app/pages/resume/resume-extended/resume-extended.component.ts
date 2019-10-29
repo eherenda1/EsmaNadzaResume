ab16f4e024f43aae9e9af462467b19b5681421fd
Nadza Buhic <nadza_buhic@zira>
2019-10-29 16:59:44 +0100

    nista nije gotovo :(

--- EsmaNadzaApp/src/app/pages/resume/resume-extended/resume-extended.component.ts
+++ EsmaNadzaApp/src/app/pages/resume/resume-extended/resume-extended.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ReadmoreService } from 'src/app/readmore.service';
+import {ResumeService } from 'src/app/resume.service';
+import { IResume } from 'src/app/resume';
+import { ActivatedRoute } from "@angular/router";
 
 
 @Component({
@@ -9,9 +12,11 @@ import { ReadmoreService } from 'src/app/readmore.service';
 export class ResumeExtendedComponent implements OnInit {
 
   public detail:any = {};
+  public resumeextended: IResume[];
   public caption: string;
+  public increment: number = 0;
 
-  constructor(private readmore:ReadmoreService) { }
+  constructor(private readmore:ReadmoreService , private _resumeService: ResumeService, private route: ActivatedRoute) { }
        
 
   ngOnInit() {
@@ -24,7 +29,42 @@ export class ResumeExtendedComponent implements OnInit {
       this.caption = data;
       console.log("naslov", this.caption);
     })
+
+    this.readmore.getItems().subscribe(resumeextended => {
+      this.resumeextended = resumeextended;
+      this.readmore.get().subscribe((resumeextended) => {
+        this.detail = resumeextended;
+        
+        if(this.detail){
+        
+        this.shift(this.resumeextended.indexOf(this.detail));
+      
+        }
+      });
+    });
+
   
 
 }
+
+
+shift(increment) {
+  if (!this.resumeextended.length || increment >= this.resumeextended.length || increment < 0) return;
+  this.detail = this.resumeextended[increment];
+
+  console.log("Detail", this.detail);
+  console.log("Drugi dio", this.resumeextended[increment])
+  
+  this.increment = increment;
+
+
+}
+
+left() {
+  this.shift(this.increment - 1);
+}
+right() {
+  this.shift(this.increment + 1);
+}
+
 }
--- EsmaNadzaApp/src/app/pages/resume/resume.component.ts
+++ EsmaNadzaApp/src/app/pages/resume/resume.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ResumeService } from 'src/app/resume.service';
 import { ReadmoreService } from 'src/app/readmore.service';
 import { ActivatedRoute } from "@angular/router";
-import { IResume } from 'src/app/resume';
+import { IResume, IHighSchool } from 'src/app/resume';
+
+import  pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 
 @Component({
@@ -14,16 +18,12 @@ import { IResume } from 'src/app/resume';
 export class ResumeComponent implements OnInit {
   public code: string;
   public resume: IResume;
-  
- 
+  public highs: IHighSchool;
 
   constructor(private _resumeService: ResumeService, private readmore:ReadmoreService, private route: ActivatedRoute) {
 
   }
   
- 
-  
-
   ngOnInit(){
     this.route.parent.params.subscribe((params:any) => {
       this.code = params.code;
@@ -32,8 +32,14 @@ export class ResumeComponent implements OnInit {
 
    this._resumeService.getResume(this.code).subscribe((data)=> {
      this.resume = data;
+     this.highs = this.resume.highSchool;
      
    });
+
+   this._resumeService.getResume(this.code).subscribe((data) => {
+    this.readmore.setItems(this.resume = data);
+    console.log("ovdje ovdje", data);
+  });
 
    
   // this._resumeService.getResume(this.code).subscribe((data)=> this.resume.work = data);
@@ -48,8 +54,32 @@ export class ResumeComponent implements OnInit {
 
   selecttitle(title){
     this.readmore.settitle(title);
+    
   
   }
+
+  getDocumentDefinition() {
+    sessionStorage.setItem('resume', JSON.stringify(this.resume));
+    return {
+      content: 'This is a sample PDF'
+    };
+  }
+
+  generatePdf(){
+    const documentDefinition = { content: [
+      {
+        text: 'RESUME',
+          bold: true,
+          fontSize: 20,
+          alignment: 'center',
+
+      },
+      
+    ]
+    };
+    pdfMake.createPdf(documentDefinition).open();
+   }
+  
 
   
   
